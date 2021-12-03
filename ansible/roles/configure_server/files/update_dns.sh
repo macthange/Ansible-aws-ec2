e9135b5f519b0d0aaf2905772b44fdd25af7a58d
Machhindra Thange <macthange@gmail.com>
2021-12-03 11:06:27 +0000

    Update update_dns.sh

--- ansible/roles/configure_server/files/update_dns.sh
+++ ansible/roles/configure_server/files/update_dns.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i '/search/c\search avivaaws.com avivagroup.com via.novanet' /etc/resolv.conf
+sed -i '/search/c\search abc.com  xyz.net' /etc/resolv.conf
 umount /var/lib/nfs/rpc_pipefs
 mount -t rpc_pipefs -o rw,relatime,nodev  sunrpc /var/lib/nfs/rpc_pipefs
